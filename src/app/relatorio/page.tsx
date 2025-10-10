d02408f0a95cf982d8e7086e2efe82728999b6a1
MatheusLBatista <matheusifro2020@gmail.com>
2025-10-09 21:26:55 -0400

    feat: introduction to nuqs

---
+++ src/app/relatorio/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { Suspense } from 'react';
+import { Relatorio } from './Relatorio';
+
+export default function RelatorioPage() {
+ 
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+      <Relatorio />
+    </Suspense>
+  )
+}
